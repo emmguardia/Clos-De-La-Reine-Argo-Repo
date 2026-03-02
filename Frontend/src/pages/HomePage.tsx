afbbfc806b72cc4f3ef5ba4ff18d02b8c4545c23
emmguardia <emonnet-mata@guardiaschool.fr>
2026-03-02 12:12:11 +0100

    home

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -127,7 +127,7 @@ export default function HomePage() {
                 Pièces phares
               </h2>
               <p className="text-gray-900 mt-3 max-w-xl">
-                Découvrez notre dernière collection Noblesse.
+                Découvrez notre dernière collection phares Back to School.
               </p>
             </div>
             <Link
